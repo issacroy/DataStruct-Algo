a17540cbe878752864151e0a2b79ca75c0916847
Issac Roy <54503102+issacroy@users.noreply.github.com>
2019-10-17 15:51:16 +0530

    Add files via upload

--- BinaryTree_Avl.cpp
+++ BinaryTree_Avl.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 class Avl
 {
 private:
@@ -9,25 +10,26 @@ private:
 		int value;
 		Node *left;
 		Node *right;
-		Node(int value):value(value),left(nullptr),right(nullptr)
+		Node(int value):value(value), left(nullptr), right(nullptr)
 		{}
 	};
 
-	Node *root;
-	Avl():root(nullptr)
-	{}
+	Node *root;//pointer for passing node adddress
 
-	Node *add(Node *node,int value)
-	{
+	//Adding the nodes into the tree
+	Node *add(Node *node, int value)
+	{ 
 		if(nullptr==node)
 			return new Node(value);
-		if(value>node->value)
+		if(value > node->value)
 			node->right =add(node->right, value);
 		else
-			node->left=add(node->left,value);
+			node->left=add(node->left, value);
+		//calling Rebalance function
 		node=Rebalance(node);
 		return node;
 	}
+
 	void Inorder(Node*node)
 	{
 		if(node)
@@ -38,43 +40,52 @@ private:
 		}
 	}
 
+	//Removing the nodes from the tree
 	Node *Remove(Node *node, int value)
 	{
+		if(node==nullptr)
+			return 0;
+
 		if (value > node->value)
-			node->right=Remove(node->right,value);
+			node->right=Remove(node->right, value);
+
 		else if(value < node->value)
-			node->left=Remove(node->left,value);
+			node->left=Remove(node->left, value);
+
 		else{
 			if(nullptr == node->left && nullptr == node->right)
 			{
 				delete node;
 				return nullptr;
 			}
-			if(nullptr!=node->left && nullptr==node->right)
+			if(nullptr != node->left && nullptr == node->right)
 			{
-				Node *orphan=node->left;
+				Node *orphan = node->left;
 				delete node;
 				return orphan;
 			}
-			if(nullptr ==node->left && nullptr !=node->right)
+			if(nullptr == node->left && nullptr != node->right)
 			{
-				Node *orphan=node->right;
+				Node *orphan = node->right;
 				delete node;
 				return orphan;
 			}
 
 			Node *successor = node->right;
-			while(nullptr!=successor->left)
-				successor=successor->left;
+			while(nullptr != successor->left)
+				successor = successor->left;
 
 			node->value = successor->value;
-			node->right = Remove(node->right,value);
-			node=Rebalance(node);
+			node->right = Remove(node->right, successor->value);
+
+
 		}
+		//calling Rebalance function
+		node=Rebalance(node);
 		return node;
 	}
 
-
+	//For Displaying the output
 	void printDebug(Node*node)
 	{
 		static int level=0;
@@ -88,90 +99,101 @@ private:
 		}
 	}
 
-	int height(Node *node)
-	{
-		if(nullptr == node)
-			return 0;
-		int leftheight = height(node->left);
-		int rightheight = height(node->right);
-		int h = max(leftheight,rightheight);
-		return h+1;
-	}
-	int balance(Node *node)
-	{
-		if(node)
-			return height(node->right)-(node->left);
-		else
-			return 0;
-	}
-
+	//Solution for Right-Right Heavy
 	Node *RotateLeft(Node *node)
 	{
 		Node *child = node->right;
-		Node *grandchild = child->left;
-		node->right=grandchild;
+		node->right=child->left;
 		child->left=node;
 		return child;
 	}
 
+	//Solution for Left-Left Heavy
 	Node *RotateRight(Node *node)
 	{
 		Node *child = node->left;
-		Node *grandchild = child->right;
-		node->left = grandchild;
-		grandchild->right = node;
+		node->left = child->right;
+		child->right = node;
 		return child;
 	}
 
+	//Solution for Right-Left Heavy
 	Node *RotateRightLeft(Node *node)
 	{
-		Node *child=node->right;
-		Node *grandchild=child->left;
-		child->right=node;
-		return child;
+		node->right = RotateRight(node->right);
+		node = RotateLeft(node);
+		return node;
+
 	}
 
+	//Solution for Left-Right Heavy
 	Node *RotateLeftRight(Node *node)
 	{
-		Node *child = node->left;
-		Node *grandchild = child->left;
-		child->right=node;
-		return child;
+		node->left = RotateLeft(node->left);
+		node = RotateRight(node);
+		return node;
+
 	}
+
+	//Finding the height values of node
+	int height(Node *node)
+	{
+		if(!node)
+			return 0;
+		int leftheight = height(node->left);
+		int rightheight = height(node->right);
+		int h = max( leftheight, rightheight);
+		return h+1;
+	}
+
+	//Finding the balance of the node usinf height
+	int balance(Node *node)
+	{
+		if(node)
+			return height(node->right)- height(node->left);
+		else
+			return 0;
+	}
+
+	//Rebalancing the node if it got imbalance by using balance function and height function
 	Node *Rebalance(Node *node)
 	{
 		int nodeBalance = balance(node);
-		if (nodeBalance == +2)
+		if (nodeBalance == +2)//check condition if its right
 		{
 			int childBalance = balance(node->right);
-			if (childBalance == +1)
+			if (childBalance == +1)//checking condition for Right-Right or Right-Left
 				node = RotateLeft(node);
 			else
 				node = RotateRightLeft(node);
 		}
-		else if (nodeBalance == -2)
+		else if (nodeBalance == -2)//check condition if its left
 		{
 			int childBalance = balance(node->left);
-			if(childBalance == -1)
+			if(childBalance == -1)//checks condition for Left-Left or LeftRight
 				node = RotateRight(node);
 			else
 				node = RotateLeftRight(node);
 		}
 		return node;
-
 	}
+
 public:
+
+
+	//Abstraction method calling class functions 
+	//using another function
 	Avl():root(nullptr)
 	{}
-	void Addnode(Node *node,int value)
+	void Addnode(int value)
 	{
-		root=add(root,value);
+		root = add(root, value);
 	}
 	void dele(int value)
 	{
-		root=Remove(root,value);
+		root=Remove(root, value);
 	}
-	void Print(Node *node)
+	void Print()
 	{
 		printDebug(root);
 	}
@@ -182,20 +204,23 @@ int main()
 {
 	Avl B;
 	int value,n;
-	B.root=nullptr;
+
+	//inserting the nodes
 	while(cout<<"enter the value (0 to stop)",
 		cin>>value,
 		value!=0)
 	{
-		B.root=B.dele(value);
-		B.Print(B.root);
+		B.Addnode(value);
+		B.Print();
 	}
+
+	//entering nodes to remove
 	while(cout<<"enter the value  to remove(0 to stop)",
 		cin>>n,
 		n!=0)
 	{
-		B.root=B.dele(n);
-		B.Print(B.root);
+		B.dele(n);
+		B.Print();
 	}
 	return 0;
 }
